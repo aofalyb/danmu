a3cb84229a0b2793d9f480245dc3e3debf207b2f
pleasecheckhere2016 <707748808@qq.com>
2018-08-27 16:00:44 +0800

    -

---
+++ barrage-mover/src/main/java/com/barrage/elastic/EsClient.java
@@ -0,0 +1,130 @@
+package com.barrage.elastic;
+
+
+import com.barrage.common.Log;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.admin.indices.exists.types.TypesExistsResponse;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+
+public class EsClient {
+
+    private EsClient() {}
+
+    private volatile static EsClient esClient;
+
+    private static TransportClient client;
+
+    private static Object lock = new Object();
+
+
+
+    public static  EsClient getInstance() {
+        if(esClient == null) {
+            synchronized (lock) {
+                if(esClient == null) {
+                    esClient = new EsClient();
+                }
+            }
+        }
+        return esClient;
+    }
+
+    public static void shutdown() {
+        if(client != null) {
+            client.close();
+        }
+
+    }
+
+    final String TYPE_NAME = "barrage";
+
+    final String INDEX_NAME = "douyu-barrage_v3";
+    public void init() throws Exception{
+        long _s = System.currentTimeMillis();
+        if(client == null) {
+            Settings settings = Settings.builder().build();
+            client  = new PreBuiltTransportClient(settings)
+                    .addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+        }
+
+
+
+        IndicesExistsResponse existsResponse = client.admin().indices().prepareExists(INDEX_NAME).execute().actionGet();
+
+        Log.errorLogger.error("existsResponse="+existsResponse.isExists());
+        if(!existsResponse.isExists()) {
+            //创建
+
+            CreateIndexResponse indexResponse = client.admin().indices().prepareCreate(INDEX_NAME).execute().actionGet();
+            Log.errorLogger.error("indexResponse"+indexResponse.isAcknowledged());
+
+        }
+
+        //索引字段
+
+        TypesExistsResponse typesResponse = client.admin().indices().prepareTypesExists(INDEX_NAME).setTypes(TYPE_NAME).execute().actionGet();
+        Log.errorLogger.error("TypesExistsResponse="+typesResponse.isExists());
+        if(!typesResponse.isExists()) {
+            // 创建索引映射,相当于创建数据库中的表操作
+            XContentBuilder mapping = XContentFactory.jsonBuilder().startObject().startObject("properties")
+                    .startObject("uid").field("type", "keyword").endObject()
+                    .startObject("nn").field("type", "keyword").endObject()
+                    .startObject("text").field("type", "keyword").endObject()
+                    .endObject().endObject();
+
+            PutMappingRequest mappingRequest = Requests.putMappingRequest(INDEX_NAME).type(TYPE_NAME).source(mapping);
+
+            Log.errorLogger.error("mapping="+client.admin().indices().putMapping(mappingRequest).actionGet().isAcknowledged());
+
+        }
+
+
+        long _e = System.currentTimeMillis();
+        System.out.println(_e - _s);
+    }
+
+
+    public boolean insert(String uid,String nn,String text) throws Exception {
+
+        long _s = System.currentTimeMillis();
+
+        IndexResponse response = client.prepareIndex(INDEX_NAME, TYPE_NAME)
+                .setSource(XContentFactory.jsonBuilder().startObject()// 开始
+                        .field("uid", uid)// 商品名称
+                        .field("nn", nn)// 商品价格
+                        .field("text", text)
+                        .endObject()).setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE).get();
+
+
+
+        long _e = System.currentTimeMillis();
+        System.out.println("insert cost time="+(_e - _s)+"ms, result="+(response.getResult() == DocWriteResponse.Result.CREATED)+".");
+        System.out.println(response);
+
+        return response.getResult() == DocWriteResponse.Result.CREATED;
+    }
+
+
+
+}
+
+
+
